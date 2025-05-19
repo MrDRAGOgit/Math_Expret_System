15599a3026193d77ec504a0dce0316497f6e300b
MrDRAGO <elitplaer@gmail.com>
2025-05-19 22:09:05 +0300

    Restructured to use classes

--- main.py
+++ main.py
@@ -1,85 +1,65 @@
 import tkinter as tk
 from tkinter import *
-
-
-# def btn_print():
-#     print(entry.get())
-#
-#
-# root = Tk()
-#
-# entry = Entry()
-# button = Button(text="click", command=btn_print)
-#
-# entry.pack()
-# button.pack()
-#
-# root.mainloop()
 from tkinter.filedialog import askopenfilename, asksaveasfilename
 
 Input = ""
+EditorOpen = False
 
 
-# def open_file():
-#     """Open a file for editing."""
-#     filepath = askopenfilename(
-#         filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")]
-#     )
-#     if not filepath:
-#         return
-#     txt_edit.delete("1.0", END)
-#     with open(filepath, mode="r", encoding="utf-8") as input_file:
-#         text = input_file.read()
-#         txt_edit.insert(END, text)
-#     window.title(f"Simple Text Editor - {filepath}")
+class KeyEditor(Toplevel):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.protocol("WM_DELETE_WINDOW", self.close_editor)
+        self.title("Addition of key words")
+    def close_editor(self):
+        self.destroy()
+        global EditorOpen
+        EditorOpen = False
 
 
-# def save_file():
-#     """Save the current file as a new file."""
-#     filepath = asksaveasfilename(
-#         defaultextension=".txt",
-#         filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")],
-#     )
-#     if not filepath:
-#         return
-#     with open(filepath, mode="w", encoding="utf-8") as output_file:
-#         text = txt_edit.get("1.0", END)
-#         output_file.write(text)
-#     window.title(f"Simple Text Editor - {filepath}")
+class MainApp(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Math Expert System")
+        txt_edit = Text(self, width=40, height=10)
+        showkeys = LabelFrame(self, width=40, height=10)
+        keysbase = ["", "", ""]
+        keysbase[0] = Label(showkeys, text="agdfiafi")
+        frm_buttons = Frame(self, relief=RAISED, bd=2)
+        btn_input = Button(frm_buttons, text="input", command=self.read_input)
+        btn_analyze = Button(frm_buttons, text="analyze", command=self.analyze)
+        btn_call_keyeditor = Button(frm_buttons, text="edit keys", command=self.open_editor)
+
+        btn_input.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
+        btn_analyze.grid(row=0, column=1, sticky="ew", padx=5, pady=5)
+        btn_call_keyeditor.grid(row=0, column=2, sticky="ew", padx=5, pady=5)
+
+        frm_buttons.grid(row=1, column=0, sticky="ns")
+        txt_edit.grid(row=0, column=0, sticky="nsew")
+
+    def open_editor(self):
+        global EditorOpen
+        editor = KeyEditor(self)
+        if not EditorOpen:
+            EditorOpen = True
+            editor.grab_set()
+
+    def read_input(self):
+        global Input
+        Input = self.txt_edit.get("1.0", END)
+
+    def analyze(self):
+        global Input
+        if "abc" in Input:
+            print("yes")
+        else:
+            print("no")
 
 
-def read_input():
-    global Input
-    Input = txt_edit.get("1.0", END)
+filepath = "keywords.txt"
+with open(filepath, mode="r", encoding="utf-8") as input_file:
+    text = input_file.read()
 
 
-def analyze():
-    global Input
-    if "abc" in Input:
-        print("yes")
-    else:
-        print("no")
-
-
-window = Tk()
-window.title("Simple Text Editor")
-
-# window.rowconfigure(0, minsize=100, weight=1)
-# window.columnconfigure(1, minsize=400, weight=1)
-
-txt_edit = Text(window, width=40, height=10)
-frm_buttons1 = Frame(window, relief=RAISED, bd=2)
-frm_buttons2 = Frame(window, relief=RAISED, bd=2)
-# btn_open = Button(frm_buttons1, text="Open", command=open_file)
-btn_input = Button(frm_buttons2, text="input", command=read_input)
-btn_analyze = Button(frm_buttons2, text="analyze", command=analyze)
-
-# btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
-btn_input.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
-btn_analyze.grid(row=0, column=1, sticky="ew", padx=5, pady=5)
-
-# frm_buttons1.grid(row=0, column=2, sticky="ns")
-frm_buttons2.grid(row=1, column=0, sticky="ns")
-txt_edit.grid(row=0, column=0, sticky="nsew")
-
+window = MainApp()
 window.mainloop()
