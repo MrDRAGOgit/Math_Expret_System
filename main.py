a7c785a1c1c02565e248d724a666042b6e9ecd6d
MrDRAGO <elitplaer@gmail.com>
2025-05-27 04:34:28 +0300

    Added Method optimisation window

--- main.py
+++ main.py
@@ -3,15 +3,23 @@ from tkinter import *
 from tkinter.filedialog import askopenfilename, asksaveasfilename
 
 EditorOpen = False
+OptimiserOpen = False
+MethodDict = {
+    "name1": 0,
+    "name2": 0,
+    "name3": 0
+}
+
 
 class KeyEditor(Toplevel):
     txt_box = ""
+
     def __init__(self, parent):
         super().__init__(parent)
         self.protocol("WM_DELETE_WINDOW", self.close_editor)
         self.title("Addition of key words")
-        self.rowconfigure(0, minsize=800, weight=1)
-        self.columnconfigure(1, minsize=800, weight=1)
+        self.rowconfigure(0, minsize=400, weight=1)
+        self.columnconfigure(1, minsize=400, weight=1)
         self.txt_box = tk.Text(self)
         frm_buttons = tk.Frame(self, relief=tk.RAISED, bd=2)
         btn_open = tk.Button(frm_buttons, text="Open", command=self.open_file)
@@ -55,6 +63,50 @@ class KeyEditor(Toplevel):
         self.title(f"Simple Text Editor - {filepath}")
 
 
+class Optimiser(Toplevel):
+    global MethodDict
+    dropmenu = ""
+    dropdowns = []
+    numdropdowns = 0
+
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.protocol("WM_DELETE_WINDOW", self.close_optimiser)
+        self.title("Algorithm optimiser")
+        self.rowconfigure(0, minsize=400, weight=1)
+        self.columnconfigure(1, minsize=400, weight=1)
+        self.dropmenu = LabelFrame(self, text="Chosen methods", width=40, height=10)
+        self.dropdowns = [OptionMenu(self.dropmenu, "Choose method", *MethodDict)]
+        btn_close = Button(self, text="Close optimiser", command=self.close_optimiser)
+
+        frm_buttons = Frame(self, relief=RAISED, bd=2)
+        btn_add = Button(frm_buttons, text="Add method", command=self.add_dropdown)
+        btn_remove = Button(frm_buttons, text="Remove method", command=self.remove_dropdown)
+        btn_add.grid(row=0, column=0, sticky="ns", padx="5", pady="5")
+        btn_remove.grid(row=0, column=1, sticky="ns", padx="5", pady="5")
+
+        self.dropmenu.grid(row=0, column=0, sticky="nsew")
+        self.dropdowns[0].grid(row=self.numdropdowns, column=0, sticky="ew", padx=5, pady=5)
+        frm_buttons.grid(row=1, column=0, sticky="ns")
+        btn_close.grid(row=1, column=1, sticky="ns", padx="5", pady="5")
+
+    def close_optimiser(self):
+        self.destroy()
+        global OptimiserOpen
+        OptimiserOpen = False
+
+    def add_dropdown(self):
+        self.dropdowns.append(OptionMenu(self.dropmenu, "Choose method", *MethodDict))
+        self.numdropdowns += 1
+        self.dropdowns[self.numdropdowns].grid(row=self.numdropdowns, column=0, sticky="ew", padx=5, pady=5)
+
+    def remove_dropdown(self):
+        self.dropdowns[self.numdropdowns].grid_remove()
+        self.dropdowns.pop()
+        self.numdropdowns -= 1
+
+
+
 class MainApp(tk.Tk):
     N = 1
     Input = ""
@@ -80,10 +132,12 @@ class MainApp(tk.Tk):
         btn_input = Button(frm_buttons, text="Input", command=self.read_input)
         btn_analyze = Button(frm_buttons, text="Analyze", command=self.analyze)
         btn_call_keyeditor = Button(frm_buttons, text="Edit keys", command=self.open_editor)
+        btn_call_optimiser = Button(frm_buttons, text="Optimise methods", command=self.open_optimiser)
 
         btn_input.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
         btn_analyze.grid(row=0, column=1, sticky="ew", padx=5, pady=5)
         btn_call_keyeditor.grid(row=0, column=2, sticky="ew", padx=5, pady=5)
+        btn_call_optimiser.grid(row=0, column=3, sticky="ew", padx=5, pady=5)
 
         self.set_keys()
 
@@ -103,6 +157,13 @@ class MainApp(tk.Tk):
         if not EditorOpen:
             EditorOpen = True
             editor.grab_set()
+
+    def open_optimiser(self):
+        global OptimiserOpen
+        optimiser = Optimiser(self)
+        if not OptimiserOpen:
+            OptimiserOpen = True
+            optimiser.grab_set()
 
     def read_input(self):
         self.Input = self.txt_edit.get("1.0", END)
